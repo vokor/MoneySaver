25350161d71ce6631d9e578962309e9d7266928c
Dolgopolova Maria <dolgopolovamariia@gmail.com>
2019-09-18 11:14:43 +0300

    screen for saving money

--- Code/MoneySaver/app/src/main/java/com/moneysaver/GoalPackge/GoalView.java
+++ Code/MoneySaver/app/src/main/java/com/moneysaver/GoalPackge/GoalView.java
@@ -1,23 +1,118 @@
 package com.moneysaver.GoalPackge;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
-import com.moneysaver.GoalPackge.Goal;
+import com.moneysaver.CreditPackage.Credit;
 import com.moneysaver.R;
 
 public class GoalView extends AppCompatActivity {
+    private Button ok;
+    private Button delete;
+    private Button save;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.goal_view);
-        Goal goal = (Goal) getIntent().getSerializableExtra(Goal.class.getSimpleName());
-        TextView name = findViewById(R.id.textViewName);
+        setContentView(R.layout.credit_view);
+        final Goal goal = (Goal) getIntent().getSerializableExtra(Goal.class.getSimpleName());
+        final EditText name = findViewById(R.id.name);
         name.setText(goal.getName());
-        TextView cost = findViewById(R.id.textViewCost);
-        cost.setText(goal.getSaved() + "/" + goal.getCost());
-        TextView notes = findViewById(R.id.textViewNotes);
+        final EditText cost = findViewById(R.id.cost);
+        cost.setText(String.valueOf(goal.getCost()));
+        final EditText notes = findViewById(R.id.notes);
         notes.setText(goal.getNotes());
+        TextView saved = findViewById(R.id.saved);
+        String str = goal.getSaved().toString();
+        final TextView savedValue = findViewById(R.id.savedValue);
+        savedValue.setText(str);
+
+        ok = findViewById(R.id.buttonOk);
+        delete = findViewById(R.id.buttonDelete);
+
+        cost.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if ((!cost.getText().toString().equals("")) && (!name.getText().toString().equals(""))) {
+                    ok.setEnabled(true);
+                } else {
+                    ok.setEnabled(false);
+                }
+            }
+        });
+
+        name.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if ((!cost.getText().toString().equals("")) && (!name.getText().toString().equals(""))) {
+                    ok.setEnabled(true);
+                } else {
+                    ok.setEnabled(false);
+                }
+            }
+        });
+
+        View.OnClickListener listenerDelete = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent();
+                i.putExtra("activity", "delete");
+                setResult(1, i);
+                finish();
+            }
+        };
+        delete.setOnClickListener(listenerDelete);
+
+        View.OnClickListener listenerOk = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newName = name.getText().toString();
+                String newNotes = notes.getText().toString();
+                String newCost = cost.getText().toString();
+
+                if(goal.getName().equals(newName) && goal.getNotes().equals(newNotes) &&
+                        goal.getCost().toString().equals(newCost)){
+                    Intent i = new Intent();
+                    i.putExtra("activity", "ok");
+                    setResult(1, i);
+                    finish();
+                }
+                else{
+                    Intent i = new Intent();
+                    i.putExtra("activity", "edit");
+                    i.putExtra("name", newName);
+                    i.putExtra("cost", newCost);
+                    i.putExtra("notes", newNotes);
+                    setResult(1, i);
+                    finish();
+                }
+            }
+        };
+        ok.setOnClickListener(listenerOk);
     }
 }
--- Code/MoneySaver/app/src/main/java/com/moneysaver/GoalPackge/SaveMoneyForGoal.java
+++ Code/MoneySaver/app/src/main/java/com/moneysaver/GoalPackge/SaveMoneyForGoal.java
@@ -8,22 +8,31 @@ import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.moneysaver.R;
 
 public class SaveMoneyForGoal extends AppCompatActivity {
-    EditText money;
-    Double balance = 0.0;
-    Button okButton;
+    private Button okButton;
+    private EditText save;
+    private double balance = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Goal goal = (Goal) getIntent().getSerializableExtra(Goal.class.getSimpleName());
         setContentView(R.layout.save_money);
-        money = findViewById(R.id.nameExpenseEdit);
+        TextView name = findViewById(R.id.name);
+        name.setText(goal.getName());
+        TextView cost = findViewById(R.id.cost);
+        cost.setText(String.valueOf(goal.getCost()));
+        TextView saved = findViewById(R.id.saved);
+        saved.setText(String.valueOf(goal.getSaved()));
+        save = findViewById(R.id.save);
+
         okButton = findViewById(R.id.buttonOk);
 
-        money.addTextChangedListener(new TextWatcher() {
+        save.addTextChangedListener(new TextWatcher() {
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
@@ -35,7 +44,7 @@ public class SaveMoneyForGoal extends AppCompatActivity {
 
             @Override
             public void afterTextChanged(Editable s) {
-                if ((Double.parseDouble(money.getText().toString()) >= 0) && (Double.parseDouble(money.getText().toString()) <= balance)) {
+                if ((Double.parseDouble(save.getText().toString()) >= 0) && (Double.parseDouble(save.getText().toString()) <= balance)) {
                     okButton.setEnabled(true);
                 } else {
                     okButton.setEnabled(false);
@@ -46,10 +55,10 @@ public class SaveMoneyForGoal extends AppCompatActivity {
         View.OnClickListener listenerOk = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Double moneyForSave = Double.parseDouble(money.getText().toString());
+                Double moneyForSave = Double.parseDouble(save.getText().toString());
 
                 Intent i = new Intent();
-                i.putExtra("money", moneyForSave);
+                i.putExtra("save", moneyForSave);
                 setResult(5, i);
                 finish();
             }
